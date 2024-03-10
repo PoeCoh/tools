4ac23bbc07d062d20ea70d6db0954fd23c84ffcf
Ben Jordan <ben@poecoh.com>
2024-03-09 22:39:28 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 # clones zig and zls git and builds from source
 # iex "& { $(irm git.poecoh.com/tools/zig/install.ps1) }"
 # to pass flags to the script, append them like this:
-# iex "& { $(irm git.poecoh.com/tools/zig/install.ps1) -Test -Legacy }"
+# iex "& { $(irm git.poecoh.com/tools/zig/install.ps1) } -Test -Legacy"
 # based off of https://github.com/ziglang/zig/wiki/Building-Zig-on-Windows
 [CmdletBinding()]
 param (
@@ -21,6 +21,7 @@ if ($PSVersionTable.PSVersion.Major -eq 5 -and -not $Legacy.IsPresent) {
     Write-Host -Object "Legacy powershell takes an eternity to download, please install and use powershell 7+."
     # No, really, legacy was shockingly slow.
     Write-Host -Object "If you must use legacy powershell, add -Legacy to the command to ignore this."
+    Write-Host -Object 'iex "& { $(irm git.poecoh.com/tools/zig/install.ps1) } -Legacy"' -ForegroundColor DarkYellow
     exit 1
 }
 
