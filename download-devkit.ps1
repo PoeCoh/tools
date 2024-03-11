83e5f37976054149d413d34c13e48ff5ed5e39db
Ben Jordan <ben@poecoh.com>
2024-03-10 22:31:47 -0400

    nevermind, stupidity

--- download-devkit.ps1
+++ download-devkit.ps1
@@ -2,11 +2,11 @@
 # Downloads devkit for version to $Env:TEMP\ziglang\devkit
 [CmdletBinding()]
 param ($RepoPath)
-# try {
+try {
     $temp = "$Env:TEMP\ziglang"
     if (-not (Test-Path -Path $temp)) { New-Item -Path $temp -ItemType Directory -Force | Out-Null }
     if (Test-Path -Path "$temp\devkit") { Remove-Item -Path "$temp\devkit" -Recurse -Force }
-    $content = Get-Content -Path "$Path\ci\x86_64-windows-debug.ps1"
+    $content = Get-Content -Path "$RepoPath\ci\x86_64-windows-debug.ps1"
     $version = ($content[1] -Split 'TARGET')[1].TrimEnd('"')
     $url = "https://ziglang.org/deps/zig+llvm+lld+clang-x86_64-windows-gnu$version.zip"
     Invoke-WebRequest -Uri $url -OutFile "$temp\devkit.zip"
@@ -14,7 +14,7 @@ param ($RepoPath)
     Rename-Item -Path (Get-ChildItem -Path $temp).where({ $_.PSIsContainer -and $_.Name -match 'zig'}).FullName -NewName "devkit"
     Get-ChildItem -Path $temp -Filter "*.zip" | Remove-Item -Recurse -Force
     return $true
-# }
-# catch {
-    # return $false
-# }
+}
+catch {
+    return $false
+}
