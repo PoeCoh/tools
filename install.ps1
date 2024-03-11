e814c297a69a38f5b6b8ba80862ae69fe27befe2
Ben Jordan <ben@poecoh.com>
2024-03-11 08:51:40 -0400

    testing

--- install.ps1
+++ install.ps1
@@ -37,15 +37,14 @@ if ($PSVersionTable.PSVersion.Major -eq 5 -and -not $Legacy.IsPresent) {
 
 if (-not (Test-Path -Path $ziglang)) { New-Item -Path $ziglang -ItemType Directory -Force | Out-Null }
 if (Test-Path -Path "$zig\.git") {
+    Write-Debug -Message "Updating zig"
     Start-Process -FilePath "git" -ArgumentList "pull", "origin" -Wait -NoNewWindow -WorkingDirectory $zig
 } else {
+    Write-Debug -Message "Cloning zig"
     Start-Process -FilePath "git" -ArgumentList "clone", "https://github.com/ziglang/zig" -Wait -NoNewWindow -WorkingDirectory $ziglang
 }
 $result = iex "& {$(irm git.poecoh.com/tools/zig/download-devkit.ps1)} -RepoPath '$zig'"
-if (-not $result) {
-    Write-Host -Object "Failed to download devkit, exiting"
-    return
-}
+if (-not $result) { throw "Failed to download devkit." }
 Write-Debug -Message "Building zig from source"
 $argList = @(
     'build'
@@ -61,23 +60,16 @@ if ($ReleaseSafe.IsPresent) { $argList += '-Doptimize=ReleaseSafe' }
 if (Test-Path -Path "$zig\stage3\bin\zig.exe") { Remove-Item -Path "$zig\stage3\bin\zig.exe" -Force }
 $building = Start-Process -FilePath "$temp\devkit\bin\zig.exe" -ArgumentList $argList -WorkingDirectory $zig -PassThru
 $building.WaitForExit()
-Write-Debug -Message "Testing: $($building.ExitCode)"
-# Set-Location -Path $zig
-# & "$temp\devkit\bin\zig.exe" build -p stage3 --search-prefix "$temp\devkit" --zig-lib-dir lib -Dstatic-llvm -Duse-zig-libcxx -Dtarget=x86_64-windows-gnu $(if ($ReleaseSafe.IsPresent) { '-Doptimize=ReleaseSafe' })
-# I need a reliable way to tell if this failed, but it seems like this always returns successful
-if (-not (Test-Path -Path "$zig\stage3\bin\zig.exe")) {
+Write-Debug -Message "Exit Code: $($building.ExitCode)"
+if ($building.ExitCode -ne 0) {
     Write-Debug -Message "Build failed, using latest release to build"
     $result = iex "& {$(irm git.poecoh.com/tools/zig/download-release.ps1)}"
-    if (-not $result) {
-        Write-Host -Object "Failed to download release, exiting"
-        return
-    }
-    Start-Process -FilePath "$temp\release\zig.exe" -ArgumentList $argList -Wait -NoNewWindow -WorkingDirectory $zig
+    if (-not $result) { throw "Failed to download release." }
+    $building = Start-Process -FilePath "$temp\release\zig.exe" -ArgumentList $argList -WorkingDirectory $zig -PassThru
+    $building.WaitForExit()
+    Write-Debug -Message "Exit Code: $($building.ExitCode)"
 }
-if (-not (Test-Path -Path "$zig\stage3\bin\zig.exe")) {
-    Write-Host -Object "Build failed, exiting cleaning up and exiting"
-    return
-}
+if ($building.ExitCode -ne 0) { throw "Zig build failed." }
 Write-Debug -Message "Build successful, adding to path"
 $paths = [Environment]::GetEnvironmentVariable('Path', 'User').Split(';').TrimEnd('\').where({ $_ -ne '' })
 if (-not $paths.Contains("$zig\stage3\bin")) {
@@ -87,11 +79,15 @@ if (-not $paths.Contains("$zig\stage3\bin")) {
 }
 Write-Debug -Message "Building zls from source"
 if (Test-Path -Path "$zls\.git") {
+    Write-Debug -Message "Updating zls"
     Start-Process -FilePath "git" -ArgumentList "pull", "origin" -Wait -NoNewWindow -WorkingDirectory $zls
 } else {
+    Write-Debug -Message "Cloning zls"
     Start-Process -FilePath "git" -ArgumentList "clone", "https://github.com/zigtools/zls" -Wait -NoNewWindow -WorkingDirectory $ziglang
 }
-Start-Process -FilePath "$zig\stage3\bin\zig.exe" -ArgumentList 'build', '-Doptimize=ReleaseSafe' -Wait -NoNewWindow -WorkingDirectory $zls
+$building = Start-Process -FilePath "$zig\stage3\bin\zig.exe" -ArgumentList 'build', '-Doptimize=ReleaseSafe' -WorkingDirectory $zls -PassThru
+$building.WaitForExit()
+Write-Debug -Message "Exit Code: $($building.ExitCode)"
 Write-Debug -Message "Adding zls to path"
 $paths = [Environment]::GetEnvironmentVariable('Path', 'User').Split(';').TrimEnd('\').where({ $_ -ne '' })
 if (-not $paths.Contains("$zls\zig-out\bin")) {
